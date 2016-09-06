b0ace53e51c2357dfdda8122d21ad4481029275c
aime1 <charles.at.nctu@gmail.com>
2016-09-06 14:33:06 -0700

    show all the png and log file in latest at first

--- server_example/server.js
+++ server_example/server.js
@@ -29,7 +29,27 @@ fs.readdirSync(latestFolderAbsPath).forEach(function(file) {
     //console.log(file_extension);
     if(file_extension == "png" || file_extension == "log" || file_extension == "latest"){
         console.log("Deleting " + file_path)
-        fs.unlinkSync(file_path);
+        var workerProcess2 = child_process.exec('rm ' + file_path,
+            function (error, stdout, stderr) {
+                if (error) {
+                    console.log(error.stack);
+                    console.log('Error code: '+error.code);
+                    console.log('Signal received: '+error.signal);
+                }
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+            });
+
+        workerProcess2.on('exit', function (code) {
+            console.log('Child process exited with exit code '+code);
+            form.on('error', function(err) {
+                console.log('error: \n' + err);
+            });
+            form.on('end', function() {
+                res.end('success');
+            });
+            form.parse(req);
+        });
     }
 });
 
@@ -135,31 +155,31 @@ httpApp.post('/upload', function(req, res){
 
     // Chu-Chi: without the following code ... it's not working ... no ideas ...
     //
-    var workerProcess2 = child_process.exec('node -v ',
-        function (error, stdout, stderr) {
-            if (error) {
-                // console.log(error.stack);
-                // console.log('Error code: '+error.code);
-                // console.log('Signal received: '+error.signal);
-            }
-            // console.log('stdout: ' + stdout);
-            // console.log('stderr: ' + stderr);
-        });
-
-    workerProcess2.on('exit', function (code) {
-        // console.log('Child process exited with exit code '+code);
-
-        form.on('error', function(err) {
-            // console.log('error: \n' + err);
-        });
-
-        form.on('end', function() {
-            res.end('success');
-        });
-
-        form.parse(req);
-
-    });
+    // var workerProcess2 = child_process.exec('node -v ',
+    //     function (error, stdout, stderr) {
+    //         if (error) {
+    //             // console.log(error.stack);
+    //             // console.log('Error code: '+error.code);
+    //             // console.log('Signal received: '+error.signal);
+    //         }
+    //         // console.log('stdout: ' + stdout);
+    //         // console.log('stderr: ' + stderr);
+    //     });
+    //
+    // workerProcess2.on('exit', function (code) {
+    //     // console.log('Child process exited with exit code '+code);
+    //
+    //     form.on('error', function(err) {
+    //         // console.log('error: \n' + err);
+    //     });
+    //
+    //     form.on('end', function() {
+    //         res.end('success');
+    //     });
+    //
+    //     form.parse(req);
+    //
+    // });
 
 
 
